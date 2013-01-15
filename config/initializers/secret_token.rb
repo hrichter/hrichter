c62a656b3d459ec4c97eb73bcbbe02736cadc79b
HRichter <h.richter@ig-office-systems.de>
2013-01-15 19:00:15 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hjb::Application.config.secret_token = '0e7367deecef52350e093bfb8eb17d7cad8ab0de27ac1955ba60f55e1edb40efa373b52c5732dae0f4e841562cb242a5a9acc3127e3e30a14e687743075e9d83'
